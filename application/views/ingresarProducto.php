4a10e53d3379f85ff0ebe8bd722848602022398f
martinmoralesm <martinhernan.93@gmail.com>
2016-07-07 18:18:25 -0400

    j

--- application/views/ingresarProducto.php
+++ application/views/ingresarProducto.php
@@ -8,9 +8,9 @@
     <br/>
     <h10 class="campoenrojo">*</h10>Nombre: <input style="color:black;margin-left: 15%; margin-bottom:2%; width: 36%;border-radius: 6px" type="text" name="nombrePro" class="input" required="true" />
     <br/>
-    <h10 class="campoenrojo">*</h10>Precio: <input type="text" style="color:black;margin-left: 15%; margin-bottom:2%; width: 36%;border-radius: 6px" name="precioPro" class="input" required="true" />
+    <h10 class="campoenrojo">*</h10>Precio: <input type="text" style="color:black;margin-left: 15%; margin-bottom:1%; width: 39%;border-radius: 6px" name="precioPro" class="input" required="true" />
     <br/>
-    <h10 class="campoenrojo">*</h10>Stock: <input type="text" style="color:black;margin-left: 15%; margin-bottom:2%; width: 36%;border-radius: 6px" name="stockPro" class="input" required="true" />
+    <h10 class="campoenrojo">*</h10>Stock: <input type="text" style="color:black;margin-left: 15%; margin-bottom:2%; width: 35%;border-radius: 6px" name="stockPro" class="input" required="true" />
     <br/>
     <h10 class="campoenrojo">*</h10>Categoria: <input type="text" style="color:black;margin-left: 15%; margin-bottom:2%; width: 36%;border-radius: 6px" name="categoriaPro" class="input" required="true" />
     <br/>

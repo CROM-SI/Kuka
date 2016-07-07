c8fddf559914629b33761cdee2b2f7dcda23b33a
martinmoralesm <martinhernan.93@gmail.com>
2016-07-07 18:14:06 -0400

    c1

--- application/views/ingresarProducto.php
+++ application/views/ingresarProducto.php
@@ -2,15 +2,17 @@
 <div id="form">
     
     <h3>Ingresar Producto</h3>
+    <h5>Los campos marcados en <h10 class="campoenrojo">*</h10> son obligatorios</h5>
+    <br/>
     
     <br/>
-    Nombre: <input type="text" name="nombrePro" class="input" required="true" />
+    <h10 class="campoenrojo">*</h10>Nombre: <input style="color:black;margin-left: 15%; margin-bottom:2%; width: 36%;border-radius: 6px" type="text" name="nombrePro" class="input" required="true" />
     <br/>
-    Precio: <input type="text" name="precioPro" class="input" required="true" />
+    <h10 class="campoenrojo">*</h10>Precio: <input type="text" style="color:black;margin-left: 15%; margin-bottom:2%; width: 36%;border-radius: 6px" name="precioPro" class="input" required="true" />
     <br/>
-    Stock: <input type="text" name="stockPro" class="input" required="true" />
+    <h10 class="campoenrojo">*</h10>Stock: <input type="text" style="color:black;margin-left: 15%; margin-bottom:2%; width: 36%;border-radius: 6px" name="stockPro" class="input" required="true" />
     <br/>
-    Categoria: <input type="text" name="categoriaPro" class="input" required="true" />
+    <h10 class="campoenrojo">*</h10>Categoria: <input type="text" style="color:black;margin-left: 15%; margin-bottom:2%; width: 36%;border-radius: 6px" name="categoriaPro" class="input" required="true" />
     <br/>
     
     <button id="ingresarPro" class="btn btn-succes">Guardar</button>

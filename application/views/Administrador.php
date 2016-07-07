b1331bbdd5d62e63d570df7ec7355ec35a524a68
respinozag <r.espinozagarrido@gmail.com>
2016-07-07 18:58:13 -0400

    botonesyheader

--- application/views/Administrador.php
+++ application/views/Administrador.php
@@ -4,25 +4,15 @@
     <br>
     <br>
 
-    <a href="#" id="btn_regBod" class="btn btn-succes" onclick="">Registrar Nuevo Encargado de bodega </a>
+    <a href="#" id="btn_regBod" onclick="">Registrar Nuevo Encargado de bodega </a>
     <br/><br/>
     
-    <a href="#" id="btn_regCli" class="btn btn-succes" onclick="cargarRegistroCli()" >Registrar nuevo Cliente</a>
+    <a href="#" id="btn_regCli" class="botonesMenu" onclick="cargarRegistroCli()" >Registrar nuevo Cliente</a>
     <br><br>
-    <a href="#" id="btn_Prod" class="btn btn-succes" onclick="cargarIngresaPro()">Ingresar Productos </a>
+    <a href="#" id="btn_Prod" onclick="cargarIngresaPro()">Ingresar Productos </a>
     <br/><br/> <br/><br/>
-    <a id="salirAdm"  href="<?=base_url()?>Controlador/salir">Salir</a>
+        <a id="salirAdm"  href="<?=base_url()?>Controlador/salir">Salir</a>
 </div>
 
 <div id="menuAdm"></div>
 
-<script>
-    $("#header").accordion({
-        collapsible: true,
-        heightStyle: "content",
-        active: 0,
-        icons: {"header": "ui-icon-plus", "activeHeader": "ui-icon-minus"}
-
-    });
-    $("#tablaInicio").html('');
-</script>

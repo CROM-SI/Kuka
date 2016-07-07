fe390c026bf7c2fdb7e484b24ef1f6e9a598c5d3
martinmoralesm <martinhernan.93@gmail.com>
2016-07-07 19:32:56 -0400

    j

--- application/views/contenido2.php
+++ application/views/contenido2.php
@@ -1,7 +1,7 @@
 <div id="contenido2">
     <h4  style="text-align: center;">Por favor, inicie sesión</h4>
     <br>
-    Usuario : <input style="color:black;margin-left: 5%; margin-bottom:2%; width: 20%;border-radius: 6px" type="text" id="nickname2" placeholder="Usuario..."><br>
-    Clave : <input style="color:black;margin-left:6%; margin-bottom:2%; width: 20%;border-radius: 6px" type="password" id="password2" placeholder="Clave..."><br>
-    <button id="btn_login2">Conectar</button>
+    Usuario  <input style="color:black;margin-left: 5%; margin-bottom:2%; width: 20%;border-radius: 6px" type="text" id="nickname2" placeholder="Usuario..."><br>
+    Clave  <input style="color:black;margin-left:6%; margin-bottom:2%; width: 20%;border-radius: 6px" type="password" id="password2" placeholder="Clave..."><br>
+    <button class="btn btn-succes btn-xs" id="btn_login2">Conectar</button>
 </div>

95a482dbe92ddc05cd575b785236a36b82ac45b2
martinmoralesm <martinhernan.93@gmail.com>
2016-07-08 12:33:48 -0400

    chupalo

--- application/views/historia.php
+++ application/views/historia.php
@@ -2,7 +2,7 @@
 
 <div class="col-md-4">
     <h3 style="color: black; letter-spacing: 5px; font-weight: bolder; margin-left: 10%;"  >Nuestra Historia</h3>
-    <br>
+    
     <h4 class="text-justify" style="margin-left: 10%;  ">Todo empezo cuando un culiao
     se le ocurrio hacer esta wea y ahora estamos hasta el pico programando chao</h4>
 </div>

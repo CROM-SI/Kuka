55ba4c35a81292b7be237e2ff87a7f021f74d2a6
martinmoralesm <martinhernan.93@gmail.com>
2016-07-08 02:37:25 -0400

    cambios de ineterfaz

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -4,23 +4,23 @@
             <h3 style="text-align: center; letter-spacing: 5px; font-weight: bolder; color: white; margin-top: 4px; margin-bottom: 6%">Tienda de Licores Kukarrox</h3>
             <div class="row">
                 <div class="col-md-4">
-<!--                    class="textodeinicio-->
-<h4 style=" margin-left: 40px; color:white;">Productos Destacados en local</h4> 
-<h1 style="margin-left: 1px;" id="sixpack">six</h1> 
+                    <!--                    class="textodeinicio-->
+                    <h4 style=" margin-left: 40px; color:white;">Lo más vendido</h4> 
+                    <h1 style="margin-left: 1px;" id="lomasvendido">six</h1> 
                 </div>
                 <div class="col-md-4">
-                    <h4 style="text-align: center; color:white;">Productos Destacados en local</h4> 
-                
-                <h1 id=jonnypromo>jonny</h1>
+                    <h4 style="text-align: center; color:white;">Productos premium</h4> 
+
+                    <h1 style="margin-left:70px;" id=premium></h1>
                 </div>
-                
+
                 <div class="col-md-4">
-                    <h4 style="margin-right:10px ; color:white">Productos Destacados en local</h4> 
-                
-                <h1 id=jonnypromo>jonny</h1>
+                    <h4 style="margin-left:170px ; color:white">Otros productos</h4> 
+
+                    <h1 id=otrosproductos>jonny</h1>
                 </div>
-            
-                
+
+
 
 
             </div>
